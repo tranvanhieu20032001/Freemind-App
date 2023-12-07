7ec35ef7e884c4067bd0d1d0fa38e67304d22cea
VanHieu <tranvanhieu20032001@gmail.com>
2023-12-07 15:47:44 +0700

    FE: Responsive leftbar

--- front-end/src/components/feed/Feed.jsx
+++ front-end/src/components/feed/Feed.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { showLeftBarContext } from "../../App";
 import './feed.css'
 
 function Feed() {
+
+  const [isScreenSmall, setIsScreenSmall] = useState(false);
+  const handleResize = () => {
+    setIsScreenSmall(window.innerWidth <= 768);
+  };
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup: loại bỏ sự kiện resize khi component bị unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const context = useContext(showLeftBarContext)
   return (
-    <div className='feed'>
+    <div className={`feed overlay ${isScreenSmall && context.showleftbar?'active':''}`}>
       <div className="feed-wrapper">
         
       </div>
